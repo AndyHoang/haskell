3ca6dad44c6e886bc13b70c86dda82c4770c178f
Andy Hoang <ngocdunghoangdl@gmail.com>
2016-04-10 17:48:13 +0700

    finished IO chapter

---
+++ learnyouahaskellforgreatgood/RPN/solveRPN.hs
@@ -0,0 +1,14 @@
+-- will support using Maybe
+import Data.List
+
+solveRPN :: String -> Float
+
+solveRPN = head . foldl foldingFunction [] . words
+  where foldingFunction (x:y:ys) "*" = (x*y) :ys
+        foldingFunction (x:y:ys) "+" = (x+y) :ys
+        foldingFunction (x:y:ys) "-" = (y-x) :ys
+        foldingFunction (x:y:ys) "/" = (y/x) :ys
+        foldingFunction (x:y:ys) "**" = (y**x) :ys
+        foldingFunction xs "sum" = [sum xs]
+        foldingFunction xs numberString = read numberString:xs
+
