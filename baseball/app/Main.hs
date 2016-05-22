53278ebd443034a1f2aabf5b2866dbeaf5848a08
Andy Hoang <ngocdunghoangdl@gmail.com>
2016-05-22 16:07:32 +0700

    just another stack sample project in this world

--- baseball/app/Main.hs
+++ baseball/app/Main.hs
@@ -1,6 +1,18 @@
 module Main where
 
 import Lib
-
+import System.Environment
 main :: IO ()
-main = someFunc
+main = do
+  commands <- getArgs
+  executeAction commands
+
+executeAction:: [String] -> IO()
+executeAction [] = sumStatsStream
+executeAction (x:_) = do
+  let (Just action) = lookup x dispatch
+  action
+
+dispatch :: [(String, IO())]
+dispatch = [("default", sumStatsStream)
+          , ("no-stream", sumStats)]
--- baseball/src/Lib.hs
+++ baseball/src/Lib.hs
@@ -1,6 +1,36 @@
 module Lib
-    ( someFunc
+  ( someFunc, sumStats, sumStatsStream
     ) where
 
+import qualified Data.ByteString.Lazy as BL
+import qualified Data.Vector as V
+-- cassava
+import Data.Csv as CSV
+import Data.Csv.Streaming as CSVS
+import Data.Foldable as F
+type BaseballStats = (BL.ByteString, Int, BL.ByteString, Int)
+
+
+sumStats :: IO()
+sumStats  = do
+  csvData <- BL.readFile "batting.csv"
+  let v = CSV.decode NoHeader csvData :: Either String (V.Vector BaseballStats)
+  let summed = fmap (V.foldr ((+).fourth) 0) v
+  putStrLn $ "Total atBats was: " ++ (show summed)
+
+fourth :: (a,b,c,d) -> d
+fourth (_,_,_,d) = d
+
+sumStatsStream :: IO ()
+sumStatsStream = do
+  csvData <- BL.readFile "batting.csv"
+  let v = CSVS.decode NoHeader csvData ::CSVS.Records BaseballStats
+  let summed = F.foldr ((+).fourth) 0 v
+  putStrLn $ "Total atBats was: " ++ (show summed)
+
+
+
 someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+someFunc = putStrLn "hello11"
+
+
